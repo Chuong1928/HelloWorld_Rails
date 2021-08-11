08eff429c4dcade7622d775cc233f295fe6386a5
Chuong1928 <6789tyh@gmail.com>
2021-08-11 18:46:29 +0700

    apply change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
    config.assets.digest = true
   
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # Enable serving assets:precompileof images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.

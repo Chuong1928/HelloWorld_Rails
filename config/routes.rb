89f398007e14f945239da2de111c1eff6b36dcff
Chuong1928 <6789tyh@gmail.com>
2021-08-11 18:37:57 +0700

    Done RoR 03 - add static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  
   resources :microposts
   resources :users
   get "/" => "users#index"
+
+  match "/404", to: 'errors#not_found', via: :all
+  match "/422", to: 'errors#unprocessable', via: :all
+  match "/500", to: 'errors#internal_server_error', via: :all
   
   root 'users#index'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

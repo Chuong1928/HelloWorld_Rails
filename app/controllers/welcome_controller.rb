e6c0919fce8fb6ea41a7d806980e61f9511a4fb8
Chuong1928 <6789tyh@gmail.com>
2021-08-13 11:13:59 +0700

    ROR 04

--- app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    @friends = Friend.all
-    p @friends
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   
-  get 'errors/not_found'
-  get 'errors/internal_server_error'
+  # get 'errors/not_found'
+  # get 'errors/internal_server_error'
   
   resources :microposts
   resources :users
   get "/" => "users#index"
+  get "sign_up" => "session#new"
+  post "sign_up" => "session#create"
 
-  match "/404", to: 'errors#not_found', via: :all
-  match "/422", to: 'errors#unprocessable', via: :all
-  match "/500", to: 'errors#internal_server_error', via: :all
+  # match "/404", to: 'errors#not_found', via: :all
+  # match "/422", to: 'errors#unprocessable', via: :all
+  # match "/500", to: 'errors#internal_server_error', via: :all
   
   root 'users#index'
  

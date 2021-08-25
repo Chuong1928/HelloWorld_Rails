9ca57efe4d3bae53b5f4cb4534bdda8ea88d75b6
Chuong1928 <6789tyh@gmail.com>
2021-08-25 10:39:26 +0700

    fix send mail in heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@example.com"
+  default from: "ngoquytruong104@gmail.com"
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'ngoquytruong104@gmail.com',
+  :password             => 'Tanh1928',
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
